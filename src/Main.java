5554fdfabe7abca8e292f4b44271b8d4725d00f1
Putri Humairah <putrihumairah302@gmail.com>
2023-12-22 14:17:16 +0700

    dirapiin

--- src/Main.java
+++ src/Main.java
@@ -8,127 +8,131 @@ public class Main {
         int jumlahMatriks;
         int row;
         int column;
-        int [][][] matriks;
+        int[][][] matriks;
         int indexMatriks;
+        int pilihan;
 
-        System.out.println("Operasi matriks");
-        System.out.println("1. Penjumlahan");
-        System.out.println("2. Pengurangan");
-        System.out.println("3. Perkalian");
-        System.out.println("4. Transpose");
-        System.out.println("5. Determinan");
-        System.out.println("6. OBE");
-        System.out.print("Masukkan pilihan: ");
-        int pilihan = scanner.nextInt();
+        do {
+            System.out.println("Operasi matriks");
+            System.out.println("1. Penjumlahan");
+            System.out.println("2. Pengurangan");
+            System.out.println("3. Perkalian");
+            System.out.println("4. Transpose");
+            System.out.println("5. Determinan");
+            System.out.println("6. OBE");
+            System.out.println("0. Keluar");
+            System.out.print("Masukkan pilihan: ");
+            pilihan = scanner.nextInt();
 
-        switch (pilihan){
-            case 1:
-                System.out.print("Masukkan jumlah matriks: ");
-                jumlahMatriks = scanner.nextInt();
-                System.out.print("Masukkan jumlah baris matriks: ");
-                row = scanner.nextInt();
-                System.out.print("Masukkan jumlah kolom matriks: ");
-                column = scanner.nextInt();
+            switch (pilihan) {
+                case 1:
+                    System.out.print("Masukkan jumlah matriks: ");
+                    jumlahMatriks = scanner.nextInt();
+                    System.out.print("Masukkan jumlah baris matriks: ");
+                    row = scanner.nextInt();
+                    System.out.print("Masukkan jumlah kolom matriks: ");
+                    column = scanner.nextInt();
 
-                matriks = new int[jumlahMatriks][row][column];
-                for (indexMatriks = 0; indexMatriks < jumlahMatriks; indexMatriks++){
-                    System.out.println("Masukkan elemen-elemen matriks ke-" + (indexMatriks + 1) + ": ");
-                    for (int i = 0; i < row; i++){
-                        for (int j = 0; j < column; j++){
-                            System.out.print("Elemen baris ke-[" + (i + 1) + "] kolom ke-[" + (j + 1) + "]: ");
-                            matriks[indexMatriks][i][j] = scanner.nextInt();
+                    matriks = new int[jumlahMatriks][row][column];
+                    for (indexMatriks = 0; indexMatriks < jumlahMatriks; indexMatriks++) {
+                        System.out.println("Masukkan elemen-elemen matriks ke-" + (indexMatriks + 1) + ": ");
+                        for (int i = 0; i < row; i++) {
+                            for (int j = 0; j < column; j++) {
+                                System.out.print("Elemen baris ke-[" + (i + 1) + "] kolom ke-[" + (j + 1) + "]: ");
+                                matriks[indexMatriks][i][j] = scanner.nextInt();
+                            }
                         }
                     }
-                }
-                int[][] sumMatriks = oprmatriks.penjumlahanMatriks(matriks);
-                System.out.println("Hasil penjumlahan matriks: ");
-                oprmatriks.printMatriks(sumMatriks);
-                break;
-            case 2:
-                System.out.print("Masukkan jumlah matriks: ");
-                jumlahMatriks = scanner.nextInt();
-                System.out.print("Masukkan jumlah baris matriks: ");
-                row = scanner.nextInt();
-                System.out.print("Masukkan jumlah kolom matriks: ");
-                column = scanner.nextInt();
+                    int[][] sumMatriks = oprmatriks.penjumlahanMatriks(matriks);
+                    System.out.println("Hasil penjumlahan matriks: ");
+                    oprmatriks.printMatriks(sumMatriks);
+                    break;
+                case 2:
+                    System.out.print("Masukkan jumlah matriks: ");
+                    jumlahMatriks = scanner.nextInt();
+                    System.out.print("Masukkan jumlah baris matriks: ");
+                    row = scanner.nextInt();
+                    System.out.print("Masukkan jumlah kolom matriks: ");
+                    column = scanner.nextInt();
 
-                matriks = new int[jumlahMatriks][row][column];
-                for (indexMatriks = 0; indexMatriks < jumlahMatriks; indexMatriks++){
-                    System.out.println("Masukkan elemen-elemen matriks ke-" + (indexMatriks + 1) + ": ");
-                    for (int i = 0; i < row; i++){
-                        for (int j = 0; j < column; j++){
-                            System.out.print("Elemen baris ke-[" + (i + 1) + "] kolom ke-[" + (j + 1) + "]: ");
-                            matriks[indexMatriks][i][j] = scanner.nextInt();
+                    matriks = new int[jumlahMatriks][row][column];
+                    for (indexMatriks = 0; indexMatriks < jumlahMatriks; indexMatriks++) {
+                        System.out.println("Masukkan elemen-elemen matriks ke-" + (indexMatriks + 1) + ": ");
+                        for (int i = 0; i < row; i++) {
+                            for (int j = 0; j < column; j++) {
+                                System.out.print("Elemen baris ke-[" + (i + 1) + "] kolom ke-[" + (j + 1) + "]: ");
+                                matriks[indexMatriks][i][j] = scanner.nextInt();
+                            }
                         }
                     }
-                }
 
-                int[][] subMatriks = oprmatriks.penguranganMatriks(matriks);
-                System.out.println("Hasil pengurangan matriks: ");
-                oprmatriks.printMatriks(subMatriks);
-                break;
-            case 3:
-                System.out.print("Masukkan jumlah matriks: ");
-                jumlahMatriks = scanner.nextInt();
-                System.out.print("Masukkan jumlah baris matriks: ");
-                row = scanner.nextInt();
-                System.out.print("Masukkan jumlah kolom matriks: ");
-                column = scanner.nextInt();
+                    int[][] subMatriks = oprmatriks.penguranganMatriks(matriks);
+                    System.out.println("Hasil pengurangan matriks: ");
+                    oprmatriks.printMatriks(subMatriks);
+                    break;
+                case 3:
+                    System.out.print("Masukkan jumlah matriks: ");
+                    jumlahMatriks = scanner.nextInt();
+                    System.out.print("Masukkan jumlah baris matriks: ");
+                    row = scanner.nextInt();
+                    System.out.print("Masukkan jumlah kolom matriks: ");
+                    column = scanner.nextInt();
 
-                matriks = new int[jumlahMatriks][row][column];
-                for (indexMatriks = 0; indexMatriks < jumlahMatriks; indexMatriks++){
-                    System.out.println("Masukkan elemen-elemen matriks ke-" + (indexMatriks + 1) + ": ");
-                    for (int i = 0; i < row; i++){
-                        for (int j = 0; j < column; j++){
-                            System.out.print("Elemen baris ke-[" + (i + 1) + "] kolom ke-[" + (j + 1) + "]: ");
-                            matriks[indexMatriks][i][j] = scanner.nextInt();
+                    matriks = new int[jumlahMatriks][row][column];
+                    for (indexMatriks = 0; indexMatriks < jumlahMatriks; indexMatriks++) {
+                        System.out.println("Masukkan elemen-elemen matriks ke-" + (indexMatriks + 1) + ": ");
+                        for (int i = 0; i < row; i++) {
+                            for (int j = 0; j < column; j++) {
+                                System.out.print("Elemen baris ke-[" + (i + 1) + "] kolom ke-[" + (j + 1) + "]: ");
+                                matriks[indexMatriks][i][j] = scanner.nextInt();
+                            }
                         }
                     }
-                }
 
-                int[][] multiplyMatrix = oprmatriks.perkalianMatriks(matriks);
-                System.out.println("Hasil perkalian matriks: ");
-                oprmatriks.printMatriks(multiplyMatrix);
-                break;
-            case 4:
-                System.out.print("Masukkan jumlah baris matriks: ");
-                row = scanner.nextInt();
-                System.out.print("Masukkan jumlah kolom matriks: ");
-                column = scanner.nextInt();
+                    int[][] multiplyMatrix = oprmatriks.perkalianMatriks(matriks);
+                    System.out.println("Hasil perkalian matriks: ");
+                    oprmatriks.printMatriks(multiplyMatrix);
+                    break;
+                case 4:
+                    System.out.print("Masukkan jumlah baris matriks: ");
+                    row = scanner.nextInt();
+                    System.out.print("Masukkan jumlah kolom matriks: ");
+                    column = scanner.nextInt();
 
-                int[][] tMatriks = new int[row][column];
-                System.out.println("Masukkan elemen-elemen matriks:");
-                for (int i = 0; i < row; i++){
-                    for (int j = 0; j < column; j++){
-                        System.out.print("Elemen baris ke-[" + (i + 1) + "] kolom ke-[" + (j + 1) + "]: ");
-                        tMatriks[i][j] = scanner.nextInt();
+                    int[][] tMatriks = new int[row][column];
+                    System.out.println("Masukkan elemen-elemen matriks:");
+                    for (int i = 0; i < row; i++) {
+                        for (int j = 0; j < column; j++) {
+                            System.out.print("Elemen baris ke-[" + (i + 1) + "] kolom ke-[" + (j + 1) + "]: ");
+                            tMatriks[i][j] = scanner.nextInt();
+                        }
                     }
-                }
 
-                System.out.println("Matriks sebelum transpose: ");
-                oprmatriks.printMatriks(tMatriks);
+                    System.out.println("Matriks sebelum transpose: ");
+                    oprmatriks.printMatriks(tMatriks);
 
-                int[][] transposedMatrix = oprmatriks.transposeMatriks(tMatriks);
+                    int[][] transposedMatrix = oprmatriks.transposeMatriks(tMatriks);
 
-                System.out.println("Matriks setelah transpose: ");
-                oprmatriks.printMatriks(transposedMatrix);
-                break;
-            case 5:
-                System.out.print("Masukkan ukuran matriks (n x n): ");
-                int n = scanner.nextInt();
+                    System.out.println("Matriks setelah transpose: ");
+                    oprmatriks.printMatriks(transposedMatrix);
+                    break;
+                case 5:
+                    System.out.print("Masukkan ukuran matriks (n x n): ");
+                    int n = scanner.nextInt();
 
-                int[][] detMatrix = new int[n][n];
-                System.out.println("Masukkan elemen-elemen matriks: ");
-                for (int i = 0; i < n; i++){
-                    for (int j = 0; j < n; j++){
-                        System.out.print("Elemen baris ke-[" + (i + 1) + "] kolom ke-[" + (j + 1) + "]: ");
-                        detMatrix[i][j] = scanner.nextInt();
+                    int[][] detMatrix = new int[n][n];
+                    System.out.println("Masukkan elemen-elemen matriks: ");
+                    for (int i = 0; i < n; i++) {
+                        for (int j = 0; j < n; j++) {
+                            System.out.print("Elemen baris ke-[" + (i + 1) + "] kolom ke-[" + (j + 1) + "]: ");
+                            detMatrix[i][j] = scanner.nextInt();
+                        }
                     }
-                }
 
-                int determinan = oprmatriks.determinanMatriks(detMatrix);
-                System.out.println("Determinan matriks: " + determinan);
-                break;
-        }
+                    int determinan = oprmatriks.determinanMatriks(detMatrix);
+                    System.out.println("Determinan matriks: " + determinan);
+                    break;
+            }
+        } while (pilihan != 0);
     }
 }
